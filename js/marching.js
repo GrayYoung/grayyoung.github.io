3fd8628b91658a2fd493124864d0359fa3d5135b
GrayYoung <windpet@live.cn>
2016-04-14 15:22:30 +0800

    Update

--- js/marching.js
+++
@@ -1,9 +0,0 @@
-/**
- * Default for the pages that don't need modules else.
- */
-
-/* The following comment tell gulp-jshint variable define is require in another file. */
-/* global require */
-require([ './config' ], function(config) {
-	require([ 'app/controller/global' ]);
-});
