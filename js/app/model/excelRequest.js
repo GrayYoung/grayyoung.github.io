7315ceaa734f2be33b3f878a19ed4f456453e00c
GrayYoung <windpet@live.cn>
2016-05-06 12:54:31 +0800

    Optimize Media page.

--- js/app/model/excelRequest.js
+++ js/app/model/excelRequest.js
@@ -5,17 +5,30 @@
 /* The following comment tell gulp-jshint variable define is defined in another file. */ 
 /* global define */
 define(function() {
-	return function(method, url, callback) {
-		var xhRequest;
+	function createXHR() {
+		if(typeof XMLHttpRequest != 'undefined') {
+			return new XMLHttpRequest();
+		} else if(typeof ActiveXObject != 'undefined') {
+			var versions = ['MSXML2.XMLHttp.6.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp'];
 
-		if(XMLHttpRequest) {
-			xhRequest = new XMLHttpRequest();
-		} else if(ActiveXObject) {
-			xhRequest = new ActiveXObject('MSXML2.XMLHTTP.3.0');
+			for(var i = 0, len = versions.length; i < len; i++) {
+				try {
+					var xhr = new ActiveXObject(versions[i]);
+
+					arguments.callee.activeXString = versions[i];
+					return xhr;
+				} catch(error) {
+					console.log('Failed to create XMLHttpRequest: ' + versions[i]);
+				}
+			}
 		} else {
-			throw 'XHR unavailable for your browser';
+			throw new Error('No XHR object availabel.');
 		}
-		xhRequest.open(method, url, true);
+	}
+
+	return function(method, url, callback) {
+		var xhRequest = createXHR();
+
 		if(typeof Uint8Array !== 'undefined') {
 			xhRequest.responseType = "arraybuffer";
 			xhRequest.onload = function(e) {
@@ -33,13 +46,18 @@ define(function() {
 		} else {
 			xhRequest.setRequestHeader('Accept-Charset', 'x-user-defined');	
 			xhRequest.onreadystatechange = function() {
-				if(xhRequest.readyState == 4 && xhRequest.status == 200) {
-					callback(XLSX.read(xhRequest.responseBody, {
-						type : 'binary'
-					}));
+				if(xhRequest.readyState == 4) {
+					if((xhRequest.status >= 200 && xhRequest.status <= 300) || xhRequest.status == 304) {
+						callback(XLSX.read(xhRequest.responseBody, {
+							type : 'binary'
+						}));
+					} else {
+						console.log('Request was unsuccessful: ' + xhRequest.status);
+					}
 				} 
 			};
 		}
+		xhRequest.open(method, url, true);
 		xhRequest.send();
 	
 		return xhRequest;
