f8aca81013a78e90f122ecd7ea88311f6ed580a8
GrayYoung <windpet@live.cn>
2016-06-07 18:04:01 +0800

    Opitimize slides

--- js/marching.js
+++ js/marching.js
@@ -8,27 +8,29 @@ require(['./config'], function(config) {
 	require(['app/controller/global']);
 	require(['slick'], function() {
 		$(document).ready(function(e) {
-			var $slider = $('#carousel-sliding').find('.carousel-inner');
+			$('.carousel.slide').each(function() {
+				var $slider = $(this).find('.carousel-inner');
 
-			if($slider.children('.item').size() > 1) {
-				$slider.slick({
-					autoplay : true,
-					autoplaySpeed : 5000,
-					dots : false,
-					dotsClass : 'carousel-indicators autogenerate',
-					appendDots : '#banner-slides > div:last-child',
-					customPaging : function(slider, i) {
-						return '<a href="#slick-slide' + (function(index) {
-							return index < 10 ? '0' + index : index;
-						})(i) + '"><span class="sr-only">' + $('#banner-slides .item').eq(i).attr('title') + '</span></a>';
-					},
-					prevArrow : $slider.siblings('.carousel-control.left'),
-					nextArrow : $slider.siblings('.carousel-control.right'),
-					pauseOnFocus : true
-				});
-			} else {
-				$slider.siblings('.carousel-control').remove();
-			}
+				if($slider.children('.item').size() > 1) {
+					$slider.slick({
+						autoplay : true,
+						autoplaySpeed : 5000,
+						dots : true,
+						dotsClass : 'carousel-indicators',
+						appendDots : $slider.parent(),
+						customPaging : function(slider, i) {
+							return '<a href="#slick-slide' + (function(index) {
+								return index < 10 ? '0' + index : index;
+							})(i) + '"><span class="sr-only">' + $('.item', $slider).eq(i).attr('title') + '</span></a>';
+						},
+						prevArrow : $slider.siblings('.carousel-control.left'),
+						nextArrow : $slider.siblings('.carousel-control.right'),
+						pauseOnFocus : true
+					});
+				} else {
+					$slider.siblings('.carousel-control').remove();
+				}
+			});
 		});
 	});
 });
