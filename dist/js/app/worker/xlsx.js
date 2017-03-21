3dc37ed7a0e26b9475dd53ddf37272b649264612
GrayYoung <windpet@live.cn>
2017-03-21 16:52:10 +0800

    Update navigation

--- dist/js/app/worker/xlsx.js
+++
@@ -1,8 +0,0 @@
-importScripts('../../lib/jszip.js');
-importScripts('../../lib/require.js');
-
-require([ '../../config' ], function(config) {
-	require(['app/model/requests', 'app/model/excelRequest', 'xlsx' ], function(requests, excelRequest) {
-		excelRequest('GET', requests.media, postMessage);
-	});
-});
