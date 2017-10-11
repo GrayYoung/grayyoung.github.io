60ca31848ff896342005ab5866aeb25e5b70b0d9
MARCHING-PC\Gray <windpet@live.cn>
2017-10-11 23:50:10 +0800

    Bootstrap Migrating to v4

--- js/app/controller/affixNavigation.js
+++ js/app/controller/affixNavigation.js
@@ -32,7 +32,7 @@ define(['app/model/util', 'app/model/breakpoints', 'jquery', 'bootstrap'], funct
 	};
 
 	$document.ready(function() {
-		var $article = $document.find('main > article'), $outline = $('<div class="col-xl-2 col-lg-3 col-xs-12 order-12 hidden-lg-down"><div class="sticky-top" /></div>');
+		var $article = $document.find('main > article'), $outline = $('<div class="col-xl-2 col-lg-3 col-xs-12 order-12 d-none d-lg-block"><div class="sticky-top" /></div>');
 
 		$outline.children(':first').append(outlineScanner($article.children('section').addClass('col-xl-10 col-lg-9 col-xs-12 order-1'), 0));
 		$article.children('section').wrapAll('<div class="row" />').first().before($outline);
